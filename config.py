68d279feacfb2acc70ec61f6e6685e46280bedd5
God2020k <107332649+God2020k@users.noreply.github.com>
2023-08-20 17:42:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH", "fcdc178451cd234e63faefd38895c991")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6435409185:AAFYzxyD--pYrS5sjDqPEecRmlzrnD7XcRI")
-    TELEGRAM_API = 1923471
-    OWNER = os.environ.get("OWNER", "880087645")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "jasuran2p0")
+    API_HASH = os.environ.get("API_HASH", "d14e54b0791ca1f8b0f26786439e336e")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6522568638:AAH8WVUyxAXIhhKYhDPsDb9HXpSOTfgQFSU")
+    TELEGRAM_API = 17627887
+    OWNER = os.environ.get("OWNER", "5179682108")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "Professor22k")
     PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://leecher:leecher@cluster0.606mkpi.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://King:King@file.fuxewu1.mongodb.net/?retryWrites=true&w=majority")
     LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
